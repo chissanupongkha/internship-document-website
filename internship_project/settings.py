729bb313b7c7f752c134609ef61c8589ba2bef35
chissanupongkha <chissanupong.kha@gmail.com>
2026-09-10 20:34:27 +0700

    Fix ALLOWED_HOSTS, font path, and disable debug

--- internship_project/settings.py
+++ internship_project/settings.py
@@ -21,7 +21,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
